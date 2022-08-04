8fe605fdc77e96c76068d33acd3ab1d0bfe276b9
adrianapvent <adriana.e.pena@gmail.com>
2022-08-04 17:31:30 -0500

    js variables

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,17 @@ tailwind.config = {
         }
     }
 }
+const citySearchEl = document.querySelector('#form-search');
+const citySearchInputEl = document.querySelector('#city-search');
+const searchTermEl = document.querySelector('#searchTerm'); // implement it
+const noResultsEl = document.querySelector('#no-results'); // implement it
+const parentContainerEl = document.querySelector('#parent-container');
+
+// render data;
+const renderCityEl = document.querySelector('#curr-city');
+const renderTempEl = document.querySelector('#curr-temp');
+const renderWindEl = document.querySelector('#curr-wind');
+const renderHumidityEl = document.querySelector('#curr-humidity');
+const renderUvIndexEl = document.querySelector('#curr-uvindex');
+const renderCurrDateEl = document.querySelector('#curr-date');
+
